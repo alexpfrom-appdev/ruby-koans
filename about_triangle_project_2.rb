a485bc8da8ef7a329d930034d07ae581db1e12fa
alexpfrom <afrom@chicagobooth.edu>
2021-04-07 19:29:03 +0000

    Completed 157

--- about_triangle_project_2.rb
+++ about_triangle_project_2.rb
@@ -3,6 +3,13 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # You need to write the triangle method in the file 'triangle.rb'
 require './triangle.rb'
 
+def triangle(a, b, c)
+  array = [a, b, c].sort
+  raise TriangleError if array.min <= 0 || array[0]+array[1] <= array[2]
+  array.uniq!
+  array.length == 1 ? :equilateral: array.length == 2 ? :isosceles : :scalene
+end
+
 class AboutTriangleProject2 < Neo::Koan
   # The first assignment did not talk about how to handle errors.
   # Let's handle that part now.
